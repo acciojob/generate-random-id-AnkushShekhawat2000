672e7160a11c7d7b288ddbafbcdacf3590dfd8f2
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 15:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+// function makeid(l) {
+//   // write your code here
+// }
+
+// // Do not change the code below.
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
+
+
+
+
 function makeid(l) {
-  // write your code here
+  // Define characters that can be used in the random string
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  // Initialize an empty string to store the result
+  let result = '';
+
+  // Generate a random character of length l
+  for (let i = 0; i < l; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
 }
 
-// Do not change the code below.
+// Example
 const l = prompt("Enter a number.");
 alert(makeid(l));
